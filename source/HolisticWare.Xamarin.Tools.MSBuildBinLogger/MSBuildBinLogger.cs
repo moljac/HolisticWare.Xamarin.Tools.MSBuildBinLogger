5bf03c1d90118a1c9ebce50b63e051fd45c29a2f
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-06-14 09:44:20 +0200

    class forgotten

---
+++ source/HolisticWare.Xamarin.Tools.MSBuildBinLogger/MSBuildBinLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HolisticWare.Xamarin.Tools
+{
+    public class MSBuildBinLogger : Microsoft.Build.Utilities.Task
+    {
+        [Microsoft.Build.Framework.Required]
+        public string IntermediateOutputPath
+        {
+            get;
+            set;
+        }
+
+        [Microsoft.Build.Framework.Required]
+        public string FileOutputMetadataNamespaces
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            Log.LogMessage($"   mc++");
+            Log.LogMessage($"       IntermediateOutputPath          = {IntermediateOutputPath}");
+            Log.LogMessage($"       FileOutputMetadataNamespaces    = {FileOutputMetadataNamespaces}");
+
+            // enforcing proper correlation between Log errors and build results (success and/or failures)
+            return !Log.HasLoggedErrors;
+        }
+
+    }
+}
